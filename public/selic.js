fbb0ede599eb9b7f31304afed5f74732882b1652
João Pedro <40773351+Parrot02@users.noreply.github.com>
2021-12-11 14:28:32 -0300

    Update no front

--- public/selic.js
+++ public/selic.js
@@ -2,7 +2,7 @@ valor = document.querySelector('#taxa')
 
 function getSelic(){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:3000/selic", true)
+    xhr.open("GET", "/selic", true)
     xhr.onreadystatechange = function(){
         if(xhr.readyState == XMLHttpRequest.DONE){
             taxa.innerHTML = `<strong>${xhr.responseText}%</strong>`
